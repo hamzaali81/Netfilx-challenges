d87a0ad2fa832fe7ed7ba4a105a5b5aa684192f6
hamzaali <hamza54834@gmail.com>
2021-02-22 21:34:52 +0500

    UI: Netflix

---
+++ src/Component/Screen/HomeScreen/HomeScreen.js
@@ -0,0 +1,22 @@
+import React,{ useState } from 'react';
+import './homeScreen.css';
+
+import Navbar from './../Navbar/Navbar';
+ 
+
+function HomeScreen() {
+  
+    return (
+        <div className="homeScreen">
+
+            {/* Navbar */}
+            <Navbar />
+            <h1>Test</h1>
+           
+            {/* Banner */}
+            {/* Row */}
+        </div>
+    )
+}
+
+export default HomeScreen;
